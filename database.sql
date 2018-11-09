10bea9fd45f7ab383c3fcd7773f68a8ea662234b
Unknown <up201505244@fe.up.pt>
2018-11-09 12:24:03 +0000

    Database (missing comments self relation)

--- database.sql
+++ database.sql
@@ -1,33 +1,74 @@
-CREATE TABLE users (
-  username VARCHAR PRIMARY KEY,
-  password VARCHAR,
-  name VARCHAR
+
+.mode columns
+.headers on
+PRAGMA foreign_keys = ON;
+
+drop table if exists channel;
+drop table if exists story;
+drop table if exists user;
+drop table if exists userSubscriptions;
+drop table if exists comment;
+
+CREATE TABLE channel (
+  name VARCHAR PRIMARY KEY,
+  subscribers INTEGER DEFAULT 0
 );
 
-CREATE TABLE news (
+CREATE TABLE story (
   id INTEGER PRIMARY KEY,
   title VARCHAR,
-  published INTEGER, -- date when the article was published in epoch format
-  tags VARCHAR, -- comma separated tags
-  username VARCHAR REFERENCES users, -- who wrote the article
-  introduction VARCHAR,
+  published INTEGER, -- date when the story was published in epoch format
+  channel VARCHAR REFERENCES channel, -- comma separated tags
+  author VARCHAR REFERENCES user, -- who wrote the article
+  points INTEGER DEFAULT 0,
   fulltext VARCHAR
 );
 
-CREATE TABLE comments (
+CREATE TABLE user (
+  username VARCHAR PRIMARY KEY,
+  password VARCHAR,
+  points INTEGER DEFAULT 0,
+  created INTEGER -- date when the user was created in epoch format
+);
+
+CREATE TABLE comment (
   id INTEGER PRIMARY KEY,
-  news_id INTEGER REFERENCES news,
-  username VARCHAR REFERENCES users,
-  published INTEGER, -- date when news item was published in epoch format
+  story_id INTEGER REFERENCES story,
+  username VARCHAR REFERENCES user,
+  published INTEGER, -- date when comment was published in epoch format
+  points INTEGER DEFAULT 0,
   text VARCHAR
 );
 
--- All passwords are 1234 in SHA-1 format
-INSERT INTO users VALUES ("dominic", "7110eda4d09e062aa5e4a390b0a572ac0d2c0220", "Dominic Woods");
-INSERT INTO users VALUES ("zachary", "7110eda4d09e062aa5e4a390b0a572ac0d2c0220", "Zachary Young");
-INSERT INTO users VALUES ("alicia", "7110eda4d09e062aa5e4a390b0a572ac0d2c0220", "Alicia Hamilton");
-INSERT INTO users VALUES ("abril", "7110eda4d09e062aa5e4a390b0a572ac0d2c0220", "Abril Cooley");
+CREATE TABLE userSubscriptions (
+  username VARCHAR REFERENCES user,
+  channel VARCHAR REFERENCES channel
+);
 
+CREATE TABLE storyVote (
+  username VARCHAR REFERENCES user,
+  story_id INTEGER REFERENCES story,
+  type INTEGER CHECK (type = 0 OR type = 1)
+);
+
+CREATE TABLE commentVote (
+  username VARCHAR REFERENCES user,
+  comment_id INTEGER REFERENCES comment,
+  type INTEGER CHECK (type = 0 OR type = 1)
+);
+
+--TODO sub comments
+
+--INSERT INTO channel VALUES()
+
+
+-- All passwords are 1234 in SHA-1 format
+
+INSERT INTO user (username, password, created) VALUES ("dominic", "7110eda4d09e062aa5e4a390b0a572ac0d2c0220", 1507901651);
+INSERT INTO user (username, password, created) VALUES ("zachary", "7110eda4d09e062aa5e4a390b0a572ac0d2c0220", 1508074451);
+INSERT INTO user (username, password, created) VALUES ("alicia", "7110eda4d09e062aa5e4a390b0a572ac0d2c0220", 1508160851);
+INSERT INTO user (username, password, created) VALUES ("abril", "7110eda4d09e062aa5e4a390b0a572ac0d2c0220", 1508247278);
+/*
 INSERT INTO news VALUES (NULL,
   'Lorem ipsum dolor sit amet, consectetur',
   1507901651,
@@ -37,8 +78,8 @@ INSERT INTO news VALUES (NULL,
   'Aliquam justo nibh, lacinia suscipit odio nec, condimentum tincidunt urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacus mi, blandit nec dolor in, ultrices condimentum elit. Quisque interdum, ante non pellentesque viverra, ipsum velit ultrices tortor, id rhoncus orci est at augue. In hac habitasse platea dictumst. Donec dolor nisi.
 
 Suspendisse potenti. Nullam lacinia dictum massa sed sagittis. Sed id ultrices libero. Cras convallis commodo ante, quis sagittis erat vulputate et. Cras nunc lorem, mollis a nibh eget, dignissim auctor lorem. Suspendisse placerat convallis ante vitae dapibus. Donec tellus felis, tincidunt eget iaculis eget, varius non turpis. Curabitur in eros at sapien fringilla venenatis eu a risus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque eu consectetur tellus. Suspendisse vitae urna ex. Cras sit amet enim id turpis gravida lacinia a vitae lacus. Vivamus augue ante, pellentesque sed semper non, rutrum ornare ante. Orci varius.');
-
-INSERT INTO news VALUES (NULL,
+*/
+/*INSERT INTO news VALUES (NULL,
   'Donec placerat tempor ex sit amet',
   1508074451,
   'local,life',
@@ -47,7 +88,8 @@ INSERT INTO news VALUES (NULL,
   'Morbi bibendum volutpat pellentesque. In bibendum est et orci semper rhoncus. Sed cursus vel orci sed malesuada. Fusce ac dictum ligula, quis hendrerit ipsum. Proin hendrerit a.
 
 Nulla commodo eu nulla ac facilisis. Donec ante lorem, tincidunt nec interdum vulputate, fringilla a urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sapien erat, suscipit a nisl sed, molestie convallis eros. Curabitur egestas massa et metus dignissim, et vestibulum libero porttitor. Sed non metus pharetra, lobortis orci a, commodo diam. Praesent a sagittis massa, quis condimentum augue. Donec id est feugiat ipsum egestas vulputate vel in dolor. Pellentesque pretium placerat lorem, sed sodales diam molestie a. Donec dictum dui ut accumsan tempor. Nam vestibulum in erat et sagittis. Donec venenatis, ante vitae tristique tristique, nisi metus aliquet.');
-
+*/
+/*
 INSERT INTO news VALUES (NULL,
   'Vivamus fermentum dui nisi, at posuere',
   1508160851,
@@ -67,7 +109,7 @@ INSERT INTO news VALUES (NULL,
   'Duis condimentum metus et ex tincidunt, faucibus aliquet ligula porttitor. In vitae posuere massa. Donec fermentum magna sit amet suscipit pulvinar. Cras in elit sapien. Vivamus nunc sem, finibus ac suscipit ullamcorper, hendrerit vitae urna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque eget tincidunt orci. Mauris congue ipsum non purus tristique, at venenatis elit pellentesque. Etiam congue euismod molestie. Mauris ex orci, lobortis a faucibus sed, sagittis eget neque.
 
 Mauris tincidunt orci congue turpis viverra pulvinar. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque rhoncus lorem eget.');
-
+*//*
 INSERT INTO comments VALUES (NULL,
   4,
   'dominic',
@@ -87,4 +129,4 @@ INSERT INTO comments VALUES (NULL,
   'alicia',
   1508247132,
   'Phasellus at neque nec nunc scelerisque eleifend eu eu risus. Praesent in nibh viverra, posuere ligula condimentum, accumsan tellus. Vivamus varius sem a mauris finibus, ac iaculis risus scelerisque. Nullam fermentum leo dui, at fermentum tellus consequat id. Pellentesque eleifend.'
-);
+);d*/
