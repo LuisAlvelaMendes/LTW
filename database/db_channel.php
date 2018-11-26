cf35f4aac8a4f7eeba780480789229c9071875f1
Simawatt- <simaoxo@hotmail.com>
2018-11-26 18:42:43 +0000

    Missing stuff

--- database/db_channel.php
+++ database/db_channel.php
@@ -1,14 +1,14 @@
 <?php
-    include_once('../includes/database.php');
+	include_once('../includes/database.php');
 
-    function getSubscribedChannels($username){
-        $db = Database::instance()->db();
+	function getSubscribedChannels($username){
+		$db = Database::instance()->db();
 
-        $subscribedChannelsQuery = $db->prepare('SELECT channel FROM UserSubscriptions WHERE username = ?');
-            
-        $subscribedChannelsQuery->execute(array($username));
-        $subscribedChannelsNames = $subscribedChannelsQuery->fetchAll();
+		$subscribedChannelsQuery = $db->prepare('SELECT channel FROM UserSubscriptions WHERE username = ?');
+				
+		$subscribedChannelsQuery->execute(array($username));
+		$subscribedChannelsNames = $subscribedChannelsQuery->fetchAll();
 
-        return $subscribedChannelsNames;
-    }
+		return $subscribedChannelsNames;
+	}
 ?>
