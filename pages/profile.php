6517a54bfacbda99d601bdc7549e9ebff3053915
Unknown <luis.alvela@gmail.com>
2018-11-27 15:37:38 +0000

    beginning profile initial info display

--- pages/profile.php
+++ pages/profile.php
@@ -1,29 +1,25 @@
 <?php 
     include_once('../includes/session.php');
     include_once('../templates/tpl_common.php');
-?>
+    include_once('../templates/tpl_sub.php');
 
+    if(!isset($_SESSION['username'])) {
+        draw_header(null, 'NOT REDDIT');
+    } else {
+        draw_header($_SESSION['username'], 'NOT REDDIT');
+    }
 
-<!DOCTYPE html>
-<html lang="en-US">
-    <head>
-        <title>Lorem ipsum dolor sit amet</title>
-        <meta charset="utf-8">
-    </head>
-    
-    <?php if(!isset($_SESSION['username'])) {
-		draw_header(null, 'NOT REDDIT');
-	} else {
-		draw_header($_SESSION['username'], 'NOT REDDIT');
-	} ?>
-    
-    <aside id="subscriptions">
-        <!--lista de subs do usr se nao estiver logged alargar o espaço das story -->
-    </aside>
-   
-    <section id="profile_info">
-        <span class="user"></span>
-    </section>
+    draw_subscriberList();
 
-     <?php draw_footer() ?>
-</html>
+    draw_footer();
+?> 
+
+<aside id="subscriptions">
+    <!--lista de subs do usr se nao estiver logged alargar o espaço das story -->
+</aside>
+
+<section id="profile_info">
+    <span class="user"></span>
+</section>
+
+    <?php draw_footer() ?>
